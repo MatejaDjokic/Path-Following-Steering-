016a7af009c93839219c7eb9a068d462a9411b94
_p4lm4 <matejadjokic23@gmail.com>
2023-02-21 23:59:43 +0100

    end

--- sketch.js
+++ sketch.js
@@ -1,46 +1,27 @@
 
+/// Steering Behavior
+
+let vehicle;
 let path;
 
 function setup() {
-  createCanvas(400, 400);
-
-  path = createVector(200, 60);
-}
-
-function vectirProjection(a, path) {
-  let bC = path.copy().normalize();
-  let sp = a.dot(bC);
-  bC.mult(sp);
-  return bC;
+  createCanvas(windowWidth, windowHeight);
+  vehicle = new Vehicle(100, 100);
+  vehicle.vel.x = 2;
+  path = new Path(0, 200, windowWidth, 200); 
 }
 
 function draw() {
-  background(0);
-  strokeWeight(4);
-  stroke(255);
+  background(0); 
 
-  let pos = createVector(100, 200);
+  path.end.y = mouseY;
 
-  let mouse = createVector(mouseX, mouseY);
-  let a = p5.Vector.sub(mouse, pos);
+  let force = vehicle.follow(path); 
+  vehicle.applyForce(force);
 
-  //line(pos.x, pos.y, pos.x + a.x, pos.y + a.y);
-  line(pos.x, pos.y, pos.x + path.x, pos.y + path.y);
+  vehicle.edges();
+  vehicle.update();
+  vehicle.show();
 
-  let v = vectirProjection(a, path);
-
-  //strokeWeight(8);
-  //stroke(0, 0, 255);
-  //line(pos.x, pos.y, pos.x + v.x, pos.y + v.y); 
-
-  //strokeWeight(1);
-  //line(pos.x + a.x, pos.y + a.y, v.x + pos.x, v.y + pos.y);
-
-  fill(0, 255, 0);
-  noStroke();
-  circle(pos.x+a.x, pos.y+a.y, 16);
-
-  fill(255, 0, 0);
-  noStroke();
-  circle(v.x + pos.x, v.y + pos.y, 16);
+  path.show();
 }
